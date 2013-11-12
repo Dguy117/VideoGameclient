cd7be79858e63c549a866914ab52f4b5a4cdc831
saasbook <saasbook@saasbook.(none)>
2013-11-12 13:55:09 -0800

    Added VideoGameclient to repo

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,11 @@
 # This file is app/controllers/games_controller.rb
 class GamesController < ApplicationController
   def index
-    @games = game.all
+    @games = Game.all
+    end
+  def show
+    id = params[:id] # retrieve games ID from URI route
+    @games = Game.find(id) # look up game by unique ID
+    # will render app/views/movies/show.html.haml by default
     end
   end
