521117c3cfef1e89323d79c83005842ca80ebcf4
Dguy117 <dhol4890@students.vsu.edu>
2013-11-19 13:40:36 -0800

    views add and edit

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,9 +3,32 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
     end
+
+  def new
+    # default: render 'new' template
+    end
+
+  def create
+    @games = Game.create!(params[:game])
+    flash[:notice] = "#{games.title} was successfully created."
+    redirect_to games_path
+    end
+
+  def edit
+    @games = Game.find params[:id]
+    end
+
+  def update
+    @games = Game.find params[:id]
+    @games.update_attributes!(params[:game])
+    flash[:notice] = "#{@games.title} was successfully updated."
+         redirect_to games_path(@games)
+    end
+
   def show
     id = params[:id] # retrieve games ID from URI route
     @games = Game.find(id) # look up game by unique ID
     # will render app/views/movies/show.html.haml by default
     end
+
   end
